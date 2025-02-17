f9e40b8f4316e4507978315fe8b3b8988e39ebb5
grizz-bot92 <bbenoit752@gmail.com>
2025-02-17 08:29:38 -0800

    added main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print("hello World")
+from textnode import *
+
+def main():
+
+	obj1 = TextNode('HELLLO', TextType.BOLD_TEXT,'https://www.boot.dev') 
+	print(obj1)
+
+
+
+
+if __name__ == "__main__":
+	main()
